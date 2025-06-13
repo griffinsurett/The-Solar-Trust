e2f1dd1b58db7daa9224d5c7b6c0d8d7a6839693
griffinjoshs <leveloutusa@gmail.com>
2025-06-13 00:36:38 -0400

    number

--- src/content/SiteData.js
+++ src/content/SiteData.js
@@ -13,7 +13,7 @@ export const SiteData = {
 
 export const ContactData = {
     email: "info@thesolartrust.com",
-    phone: "(856) 383-9386",
+    phone: "(848) 383-9386",
 }
 
 export const SocialData = [
